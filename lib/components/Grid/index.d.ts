109dc7e5f0e7159fcf9e24544c37026e027dc942
systembugtj <albert_lee@hotmail.com>
2025-01-18 16:58:16 -0800

    Updates

--- lib/components/Grid/index.d.ts
+++ lib/components/Grid/index.d.ts
@@ -3,5 +3,7 @@ interface GridProps<T> extends React.HTMLAttributes<HTMLDivElement> {
     data: T[];
     columns: ColumnDef<T, number>[];
 }
-export declare function Grid<T>({ className, data, columns, ...restProps }: GridProps<T>): import("react/jsx-runtime").JSX.Element;
+export declare function Grid<T extends {
+    userId: string;
+}>({ className, data, columns, ...restProps }: GridProps<T>): import("react/jsx-runtime").JSX.Element;
 export {};
